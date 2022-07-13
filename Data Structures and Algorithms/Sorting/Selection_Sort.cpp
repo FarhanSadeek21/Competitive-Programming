5ffcad85fc0ba3481aee55d21752ab68f90caa13
FarhanSadeek21 <farhansadeek019@gmail.com>
2022-07-13 01:23:22 -0400

    Bubble Sort

--- Data Structures and Algorithms/Sorting/Selection_Sort.cpp
+++ Data Structures and Algorithms/Sorting/Selection_Sort.cpp
@@ -8,6 +8,8 @@ int main(){
         cin >> v[i];
     }
 
+    // * Implementation of Selection Sort
+    
     for(int i = 0; i < n; i++){
         int minIndex = i;
         for(int j = i + 1; j < n; j++){
@@ -17,6 +19,8 @@ int main(){
         }
         swap(v[i], v[minIndex]);
     }
+
+
     for(int i = 0; i < n; i++){
         cout << v[i] << " ";
     }
